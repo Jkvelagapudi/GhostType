99a5d051b23bcae8343e5a5e0a1186ada3ae475d
Jayadeep Velagapudi <111027250+Jkvelagapudi@users.noreply.github.com>
2025-04-03 22:31:43 -0500

    letting it run

--- typing_test.py
+++ typing_test.py
@@ -4,20 +4,54 @@ import subprocess
 import os
 import pyautogui
 import nltk
-
-subprocess.run(["spectacle", "-r", "-b", "-n", "-d", "5000", "-o", "screenshot.png"])
-
-
-img = cv2.imread('screenshot.png')
+from time import sleep
+from PIL import Image
 
 
-text = pytesseract.image_to_string(img)
+def take_screenshot():
+    # Take the screenshot using Spectacle
+    subprocess.run(["spectacle", "-b", "-n", "-d", "0", "-o", "screenshot.png"], check=True)
 
-words = nltk.tokenize.word_tokenize(text)
+    # Open the screenshot image using Pillow
+    screenshot = Image.open("screenshot.png")
+
+    crop_box = (300, 520, 2700, 1325)
+
+    # Crop the screenshot
+    cropped_img = screenshot.crop(crop_box)
+
+    # Save the cropped image
+    cropped_img.save("text.png")
+
+def do_test():
+    img = cv2.imread('text.png')
 
 
-for word in words:
-    pyautogui.typewrite(word)
-    pyautogui.press('space')
+    text = pytesseract.image_to_string(img)
 
-os.remove("screenshot.png")
+    words = nltk.tokenize.word_tokenize(text)
+
+    sent = ""
+
+    for word in words:
+        sent += word + " "
+
+    # pyautogui.typewrite(sent, 0.0275)
+    pyautogui.typewrite(sent, 0.03)
+
+def restart_test():
+    sleep(1)
+    pyautogui.typewrite(['tab', 'enter'])
+
+
+
+try:
+    sleep(2)
+    while True:
+        take_screenshot()
+        do_test()
+        restart_test()
+
+        
+except KeyboardInterrupt:
+    print("Stopped.")
